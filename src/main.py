3f21afb4a280d999ea229bff64f962f12f65ec89
Jorge Ahumada <grandekoke@gmail.com>
2023-10-22 00:25:40 -0300

    refactor: Enter key to exit and update the interface

--- src/main.py
+++ src/main.py
@@ -50,10 +50,13 @@ def delete_task(data, task):
         json.dump(data, file)
 
 
-def interate_projects(data):
+def iterate_projects(data):
     while True:
         os.system("cls || clear")
-        print(f"Proyecto: {data['project_name']}\n")
+        title = f"| proyecto: {data['project_name']} |".upper()
+        print(len(title) * "=")
+        print(f"{title}")
+        print(len(title) * "=", end="\n\n")
 
         print("Tareas pendientes:")
         print_tasks(data, "tasks")
@@ -68,10 +71,10 @@ Selecciona una opción:
 2. Editar una tarea (selecciona el número de la tarea)
 3. Eliminar una tarea (selecciona el número de la tarea)
 4. Marcar una tarea como completada (selecciona el número de la tarea)
-5. Salir
+5. Presiona ENTER para volver al menú principal
         """
         )
-        option = input("Ingresa una opción: ")
+        option = input("> Ingresa una opción: ")
 
         if option == "1":
             add_task(data)
@@ -95,7 +98,7 @@ Selecciona una opción:
                 update_status_task(data, data["tasks"][int(n_taks) - 1])
             else:
                 print("No hay tareas.")
-        elif option == "5":
+        elif option == "":
             print("Has seleccionado la opción 5")
             break
         else:
@@ -111,10 +114,12 @@ def create_project():
         os.system("cls || clear")
 
         # Pedir el nombre del proyecto o volver al menú principal
-        print(
-            "Ingresa el nombre del proyecto o presiona ENTER para volver al menú principal"
-        )
-        project_name = input("Nombre del proyecto: ")
+        print("""
+Crear un nuevo proyecto:
+1. Ingresa el nombre del proyecto
+2. Presiona ENTER para volver al menú principal
+""")
+        project_name = input(" > ")
 
         if project_name == "":
             return
@@ -131,22 +136,28 @@ def create_project():
         data = {"project_name": project_name, "tasks": [], "completed_tasks": []}
         json.dump(data, file)
     file.close()
-    print(f"Proyecto {project_name} creado correctamente")
+    print(f"Proyecto {project_name} creado correctamente", end=" ")
     time.sleep(2)
-    interate_projects(data)
+    iterate_projects(data)
 
 
 def initialize_project(list_projects):
     while True:
-        print("Ingresa el número del proyecto que deseas iniciar:", end=" ")
-        option = input()
+        print("""
+Iniciar un proyecto:
+ * Ingresa el número del proyecto que deseas iniciar
+ * Presiona ENTER para volver al menú principal
+""")
+        option = input("> ")
+
+        if option == "":
+            return
 
         try:
             project = list_projects[int(option) - 1]
             with open(f"projects/{project}", "r") as file:
                 text = file.read()
                 os.system("cls || clear")
-                # print(f"Iniciando el proyecto {data['project_name']}...")
             file.close()
             break
         except IndexError:
@@ -155,13 +166,14 @@ def initialize_project(list_projects):
             print("Ingresa un número")
         except Exception:
             print("Ha ocurrido un error")
-    data = json.loads(text)
-    interate_projects(data)
 
+    return json.loads(text)
 
 def show_projects():
     # Limpiar la pantalla
     os.system("cls || clear")
+
+    print("Proyectos disponibles:")
     # Si no existe la carpeta 'projects' la crea
     if not os.path.exists("projects"):
         print("No hay proyectos...", end=" ")
@@ -169,11 +181,11 @@ def show_projects():
         input("Presiona ENTER para continuar")
         return
 
-    proyects = os.listdir("projects")
-    for index, project in enumerate(proyects):
+    projects = os.listdir("projects")
+    for index, project in enumerate(projects):
         print(f"{index + 1}. {project}")
 
-    initialize_project(proyects)
+    return projects
 
 
 def main():
@@ -185,7 +197,7 @@ def main():
 Selecciona una opción:
 1. Crear un nuevo proyecto
 2. Iniciar un proyecto existente
-3. Salir
+3. Presiona ENTER para salir 
         """
         )
         option = input("Ingresa una opción: ")
@@ -193,8 +205,10 @@ Selecciona una opción:
         if option == "1":
             create_project()
         elif option == "2":
-            show_projects()
-        elif option == "3":
+            projects = show_projects()
+            project = initialize_project(projects)
+            iterate_projects(project)
+        elif option == "":
             print("Has seleccionado la opción 3")
             break
         else:
